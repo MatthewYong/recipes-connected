cab1c7877779eee4ef51b5ede36af248a8dec65a
Matthew <kfm.yong@gmail.com>
2020-09-21 20:00:15 +0000

    Add gitignore file

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def test():
@@ -12,4 +23,4 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
+
