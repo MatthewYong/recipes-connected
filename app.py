04cacd8192ae02219b8d7be4710d007605774d6e
Matthew <kfm.yong@gmail.com>
2020-09-26 20:01:38 +0000

    Add app.route for recipes, register and login pages

--- app.py
+++ app.py
@@ -21,6 +21,18 @@ mongo = PyMongo(app)
 def home():
     return render_template("index.html")
 
+@app.route('/all_recipes')
+def all_recipes():
+    return render_template("recipes.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
