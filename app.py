3fa7775076859fdbed15ad00148bbe96036a0543
Matthew <kfm.yong@gmail.com>
2020-10-03 15:13:56 +0000

    Add remove method mongo.remove in app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def add_recipe():
     return render_template("add_recipe.html")
 
 
-@app.route('/add_mongodb_recipe', methods=['POST'])
-def add_mongodb_recipe():
+@app.route('/add_recipe_mongodb', methods=['POST'])
+def add_recipe_mongodb():
     recipe = mongo.db.recipes
     recipe.insert_one(request.form.to_dict())
     return redirect(url_for('all_recipes'))
@@ -69,8 +69,8 @@ def update_recipe(recipe_id):
 
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
-    delete_recipe = mongo.db.recipes.remove({"_id": ObjectId(recipe_id)}
-    return render_template("all_recipes.html", delete_recipe)
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    return render_template("all_recipes.html")
 
 
 @app.route('/register')
